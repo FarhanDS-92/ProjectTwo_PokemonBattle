26cb6b6aaf3f4562186279c4ba1c591681acc50f
Umai Rav <umai5@hotmail.com>
2023-01-16 19:20:13 -0500

    made changes

--- app.js
+++ app.js
@@ -258,7 +258,7 @@ function getRandomInt(max) {
 // create a paragraph tag that states the winner in the header of the page
 //     (stretch goal: create a simple animation that shows the winner moving)
 
-
+console.log("hello");
 
 const button1 = document.querySelector("#continueToFight");
 const pageOne = document.querySelector(".testWrapper");
